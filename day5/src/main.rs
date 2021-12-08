10fc4dd621035e28752b98da57dd6c6d45bc64f8
Chris Penner <christopher.penner@gmail.com>
2021-12-07 22:22:04 -0600

    Finish day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -7,6 +7,7 @@ use std::{
 
 fn main() {
     day1();
+    day2();
 }
 
 struct Point {
@@ -39,14 +40,63 @@ fn day1() {
             };
             return temp;
         })
-        .inspect(|x| println!("{:?}", x))
         .fold(HashMap::new(), |mut acc, (x, y)| {
             let v: &mut u32 = acc.entry((x, y)).or_insert(0);
             *v += 1;
             return acc;
         });
 
-    println!("{:?}", coords);
+    let count = coords.iter().filter(|(_, v)| **v > 1).count();
+    println!("{}", count);
+}
+
+fn day2() {
+    let numbers = Regex::new(r"\d+").unwrap();
+    let coords: HashMap<(u32, u32), u32, RandomState> = fs::read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|ln| {
+            let ns: Vec<u32> = numbers
+                .find_iter(ln)
+                .map(|n| n.as_str().parse().unwrap())
+                .collect();
+            match ns[..] {
+                [x1, y1, x2, y2] => (Point { x: x1, y: y1 }, Point { x: x2, y: y2 }),
+                _ => unreachable!(),
+            }
+        })
+        .flat_map(|(p1, p2)| {
+            if p1.x == p2.x {
+                (min(p1.y, p2.y)..max(p1.y, p2.y) + 1)
+                    .map(move |y| (p1.x, y))
+                    .collect::<Vec<_>>()
+            } else if p1.y == p2.y {
+                (min(p1.x, p2.x)..max(p1.x, p2.x) + 1)
+                    .map(move |x| (x, p1.y))
+                    .collect::<Vec<_>>()
+            } else {
+                let xs: Vec<u32> = if p1.x < p2.x {
+                    (p1.x..p2.x + 1).collect()
+                } else {
+                    (p2.x..p1.x + 1).rev().collect()
+                };
+                let ys: Vec<u32> = if p1.y < p2.y {
+                    (p1.y..p2.y + 1).collect()
+                } else {
+                    (p2.y..p1.y + 1).rev().collect()
+                };
+                xs.iter()
+                    .zip(ys)
+                    .map(|(x, y)| (*x, y))
+                    .collect::<Vec<(u32, u32)>>()
+            }
+        })
+        .fold(HashMap::new(), |mut acc, (x, y)| {
+            let v: &mut u32 = acc.entry((x, y)).or_insert(0);
+            *v += 1;
+            return acc;
+        });
+
     let count = coords.iter().filter(|(_, v)| **v > 1).count();
     println!("{}", count);
 }
