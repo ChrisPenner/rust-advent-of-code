e501a3fa403ae70a03b4789a079efb26e17e0bd7
Chris Penner <christopher.penner@gmail.com>
2021-12-07 21:44:34 -0600

    Working day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use std::{
+    cmp::{max, min},
     collections::{hash_map::RandomState, HashMap},
     fs, iter,
 };
@@ -21,7 +22,7 @@ fn day1() {
         .map(|ln| {
             let ns: Vec<u32> = numbers
                 .find_iter(ln)
-                .map(|n| dbg!(n.as_str()).parse().unwrap())
+                .map(|n| n.as_str().parse().unwrap())
                 .collect();
             match ns[..] {
                 [x1, y1, x2, y2] => (Point { x: x1, y: y1 }, Point { x: x2, y: y2 }),
@@ -30,14 +31,15 @@ fn day1() {
         })
         .flat_map(|(p1, p2)| {
             let temp: Box<dyn Iterator<Item = (u32, u32)>> = if p1.x == p2.x {
-                Box::new((p1.y..p2.y).map(move |y| (p1.x, y)))
+                Box::new((min(p1.y, p2.y)..max(p1.y, p2.y) + 1).map(move |y| (p1.x, y)))
             } else if p1.y == p2.y {
-                Box::new((p1.x..p2.x).map(move |x| (x, p1.y)))
+                Box::new((min(p1.x, p2.x)..max(p1.x, p2.x) + 1).map(move |x| (x, p1.y)))
             } else {
                 Box::new(iter::empty())
             };
             return temp;
         })
+        .inspect(|x| println!("{:?}", x))
         .fold(HashMap::new(), |mut acc, (x, y)| {
             let v: &mut u32 = acc.entry((x, y)).or_insert(0);
             *v += 1;
@@ -45,4 +47,6 @@ fn day1() {
         });
 
     println!("{:?}", coords);
+    let count = coords.iter().filter(|(_, v)| **v > 1).count();
+    println!("{}", count);
 }
